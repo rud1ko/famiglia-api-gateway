60e7ca9669e027840075fbf1aa3e0303f078138c
Rudiko777 <chakryan_rudik@mail.ru>
2024-12-01 22:00:46 +0300

    first commit

---
+++ src/main/java/famiglia_api_gateway/FamigliaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package famiglia_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamigliaApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamigliaApiGatewayApplication.class, args);
+	}
+
+}
